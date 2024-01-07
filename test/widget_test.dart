6752cf34a0c5f841f36f0255eb2b44d6da73b2c1
LAPTOP-R0JLB3EV\Erric <erric.rapsing@gmail.com>
2024-01-07 22:12:27 +0800

    add dummy test file to continue workflow

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,12 @@
 
 // import 'package:ale/main.dart';
 
+import 'package:flutter_test/flutter_test.dart';
+
 void main() {
+  test('TODO', () {
+    expect('TODO', 'TODO');
+  });
   // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
   //   // Build our app and trigger a frame.
   //   await tester.pumpWidget(const MyApp());
